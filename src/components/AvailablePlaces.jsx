4cfe96661734331d4024b1b4ba9ec63af03ae1ec
Mayuri <mayurigurav1997@gmail.com>
2024-05-08 23:01:05 +0530

    error messages

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,22 +1,38 @@
 import { useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 // const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     setIsFetching(true);
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places,please try again later.",
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+  if (error) {
+    return <Error title="An error occured!!" message={error.message} />;
+  }
 
   return (
     <Places
